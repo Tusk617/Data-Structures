4309be77314b73bac61749a1c36ba8b0c305212c
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-07-14 17:20:16 -0400

    Completed module 1 MVP

--- stack/stack.py
+++ stack/stack.py
@@ -10,92 +10,26 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+from singly_linked_list import LinkedList
+
 class Stack:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
         return self.size
         pass
 
     def push(self, value):
-        self.storage.append(value)
-        self.size = len(self.storage)
-        pass
+        self.storage.addToTail(value)
+        self.size += 1
 
     def pop(self):
-        if self.size == 0:
-            return None
+        if self.size >= 1:
+            self.size -= 1
+
+            return self.storage.removeTail()
         else:
-            popStorage = self.storage.pop()
-            self.size = len(self.storage)
-            return popStorage
-        pass
-
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-    def getValue(self):
-        return self.value
-    def getNext(self):
-        return self.next
-    def setNext(self, newNext):
-        self.next = newNext
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-    def addToTail(self,value):
-        newNode = Node(value)
-        if self.head is None and self.tail is None:
-            self.head = newNode
-            self.tail = newNode
-        else:
-            self.tail.setNext(newNode)
-            self.tail = newNode
-    def removeHead(self):
-        if self.head is None and self.tail is None:
             return
-        if not self.head.getNext():
-            head = self.head
-            self.head = None
-            self.tail = None
-            return head.getValue()
-        val = self.head.getValue()
-        self.head = self.head.getNext()
-        return val
-    
-    def removeTail(self):
-        if self.head is None:
-            return
-        current = self.head
-        while current.getNext() and current.getNext() is not self.tail:
-            current = current.getNext()
-        value = self.tail.getValue()
-        self.tail = current
-        self.tail.setNext(None)
-        return value
 
-    def contains(self, value):
-        if not self.head:
-            return False
-    
-        current = self.head
-        while current:
-            if current.getValue() == value:
-                return True
-            current = current.getNext()
-        return False
-
-    def get_max(self):
-        if not self.head:
-            return None
-        max_value = self.head.getValue()
-        current = self.head.getNext()
-        while current:
-            if current.getValue() > max_value:
-                max_value = current.getValue()
-            current = current.getNext()
-        return max_value
